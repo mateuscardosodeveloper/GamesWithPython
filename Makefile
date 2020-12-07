0d0159dc0f47c041d69b13d186b1b25b06fd208f
unknown <mateus.informatica44@gmail.com>
2020-12-07 17:28:09 -0300

    Finished the game forca of alura course

---
+++ Makefile
@@ -0,0 +1,5 @@
+install:
+	pip install -e.
+
+hello:
+	echo "hello"
---
+++ setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+def read(filename):
+    return[
+            req.strip()
+            for req
+            in open(filename).readlines()
+    ]
+
+setup(
+    name="JogoDaVelha",
+    version="0.1.0", 
+    description="",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=read("requirements.txt")
+)
